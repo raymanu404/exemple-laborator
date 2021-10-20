ff6e3058dcf208584316eb31012291b2c1a5e44e
Emanuel Caprariu <manu.caprariu.brediceanu@gmail.com>
2021-10-20 13:40:56 +0300

    init tema2

---
+++ EmanuelCaprariu_lab2/EmanuelCaprariu_lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmanuelCaprariu_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
