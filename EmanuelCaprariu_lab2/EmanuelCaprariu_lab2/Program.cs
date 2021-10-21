43a74124a0667ce5d0ffb629d7d9bc93bc0221f0
Emanuel Caprariu <manu.caprariu.brediceanu@gmail.com>
2021-10-21 19:37:28 +0300

    putin facut partea de meniu

--- EmanuelCaprariu_lab2/EmanuelCaprariu_lab2/Program.cs
+++ EmanuelCaprariu_lab2/EmanuelCaprariu_lab2/Program.cs
@@ -11,15 +11,17 @@ namespace EmanuelCaprariu_lab2
 {
     class Program
     {
+        private static var listOfOrders;
         static void Main(string[] args)
         {
-            var listOfOrders = ReadListOfOrders().ToArray();
+
+            listOfOrders = ReadListOfOrders().ToArray();
             PlacingOrdersCommand command = new(listOfOrders);
             Domain.PlacingOrderWorkflow workflow = new Domain.PlacingOrderWorkflow();
 
             var result = workflow.Execute(command, (checkOrderExist) => true);
             result.Match(
-             
+
                 whenPlacingOrderEventFailedEvent: @event =>
                 {
                     Console.WriteLine($"Placing the order was failed : {@event.Reason}");
@@ -31,16 +33,44 @@ namespace EmanuelCaprariu_lab2
                     return @event;
                 }
                 );
-
-            foreach(var order in listOfOrders)
+            string option;
+            List<UnvalidatedCustomerOrder> listOfOrdersCopied = new List<UnvalidatedCustomerOrder>(listOfOrders);
+            do
             {
-                Console.WriteLine(order.toStringOrder());
-            }
+                Console.WriteLine();
+                menu();
+                Console.WriteLine();
+                option = ReadValue("Option :");              
+                switch (option)
+                {
+                   
+                    case "1":
+                        printThecart(listOfOrdersCopied);
+                        break;
+                    case "2":
+                        string code = ReadValue("Check code...");
+                        checkOrderExist(listOfOrdersCopied, code);
+                        break;
+                    case "3":
+                        string address = ReadValue("Check address...");
+                        checkOrderAddress(listOfOrdersCopied, address);
+                        break;
+                    case "4":
+                        string codeForStock = ReadValue("Check stock...");
+                        checkStockForSpecificOrder(listOfOrdersCopied, codeForStock);
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        break;
+                }
+                
+            } while (option != "q");
+            
         }
 
         private static List<UnvalidatedCustomerOrder> ReadListOfOrders()
         {
-            List<UnvalidatedCustomerOrder> listOfOrders = new();
+            List<UnvalidatedCustomerOrder> listOfOrders = new();           
             do
             {
                 //read registration number and grade and create a list of greads
@@ -71,8 +101,12 @@ namespace EmanuelCaprariu_lab2
                 {
                     break;
                 }
-
-
+                var more = ReadValue("more? y/n");
+                if (!more.Equals("y"))
+                {
+                    break;
+                }
+               
                 listOfOrders.Add(new(orderRegistrationCode, orderDescription, orderAmount, orderAddress, orderPrice));
             } while (true);
             return listOfOrders;
@@ -82,6 +116,63 @@ namespace EmanuelCaprariu_lab2
         {
             Console.Write(prompt);
             return Console.ReadLine();
+        }
+
+        private static string checkOrderExist(List<UnvalidatedCustomerOrder> ordersList,string orderCode)
+        {
+            foreach(var order in ordersList)
+            {
+                if (order.OrderRegistrationCode.Equals(orderCode))
+                {
+                    return $"Your order is here: {order.OrderRegistrationCode}";
+                }
+            }
+            return $"Unfortunately, your order is not in our storage or wrong code!";
+        }
+        private static void printThecart(List<UnvalidatedCustomerOrder> ordersList)
+        {
+            foreach (var order in ordersList)
+            {
+                Console.WriteLine(order.toStringOrder());
+                Console.WriteLine("-----------------------------------");
+            }
+        }
+        private static string checkOrderAddress(List<UnvalidatedCustomerOrder> ordersList,string address)
+        {
+            foreach (var order in ordersList)
+            {
+                if (order.OrderAddress.Equals(address))
+                {
+                    return $"The address is correct : {order.OrderAddress}";
+                }
+            }
+            return $"This address is invalid!";
+        }
+
+        private static string checkStockForSpecificOrder(List<UnvalidatedCustomerOrder> ordersList,string regCode)
+        {
+            foreach (var order in ordersList)
+            {
+                if (order.OrderRegistrationCode.Equals(regCode) && Decimal.Parse(order.OrderAmount) > 0)
+                {
+                   return ($"We have in storage this product {order.OrderDescription}");
+                }
+               
+            }
+            return $"Wrong code or stock exceeded";
+        }
+        private static void menu()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("0.Add more orders in your cart:");
+            Console.WriteLine("1.Print list of orders:");
+            Console.WriteLine("2.Check order by its reg code:");
+            Console.WriteLine("3.Check address of order:");
+            Console.WriteLine("4.Check stock for your order:");
+            Console.WriteLine("clear - to clear the screen");
+            Console.WriteLine("q - exit");
+            Console.WriteLine("Your option is ...");
+            
         }
     }
 }
